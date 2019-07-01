a3a4f5f469dfb77e75a1bf267aebb3f511e73f78
Irene Mutua <mutuamirene@gmail.com>
2019-07-01 17:10:28 +0300

    Styled the components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React from "react";
 class App extends React.Component {
     render() {
         return (
-            <form>
-                <label>Username</label>
-                <input type="text" placeholder="Enter your username" />
-                <br /> <br />
-                <label>Password</label>
-                <input type="password" placeholder="Type your password" />
-                <br />
-                <input type="submit" value="Log In" />
-            </form> 
+            <div className="App">
+                <h2 >Login Page</h2>
+                <form> 
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter your username" />
+                    <br /> <br />
+                    <label>Password</label>
+                    <input type="password" placeholder="Type your password" />
+                    <br /> <br />
+                    <input type="submit" value="Log In" />
+                </form>
+            </div>
 
         )
     }
