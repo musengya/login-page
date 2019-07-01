5ad8442470b3fa94b35b32c9c7192bbee7bf6f49
Irene Mutua <mutuamirene@gmail.com>
2019-07-01 17:34:57 +0300

    Added methods to the App component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,59 @@ class App extends React.Component {
         super(props);
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            error: ""
         }
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.username) {
+            return this.setState({
+                error: "Username is required"
+            });
+        }
+            if (!this.state.password) {
+                return this.setState({
+                    error: "Password is required"
+                });
+        }
+        return this.setState({
+            error: ""
+        });
+    }
+    handleUserChange(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    handlePassChange(e) {
+        this.setState({
+            password: e.target.value
+        });
     }
     render() {
         return (
             <div className="App">
                 <h2 >Login Page</h2>
-                <form> 
+                <form onSubmit={this.handleSubmit}> 
                     <label>Username</label>
-                    <input type="text" placeholder="Enter your username" />
+                    <input
+                        type="text"
+                        placeholder="Enter your username"
+                        value={this.state.username}
+                        onChange={this.handleUserChange} />
                     <br /> <br />
                     <label>Password</label>
-                    <input type="password" placeholder="Type your password" />
+                    <input
+                        type="password"
+                        placeholder="Type your password"
+                        value={this.state.password}
+                        onChange={this.handlePassChange} />
                     <br /> <br />
-                    <input type="submit" value="Log In" />
+                    <input
+                        type="submit"
+                        value="Log In" />
                 </form>
             </div>
 
